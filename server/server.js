28d15ea3ec73a3d8341efc44285523cf87c66b93
David Ogor <doctown@yahoo.com>
2016-04-07 10:30:28 -0700

    Set static serving of client/public folder

--- server/server.js
+++ server/server.js
@@ -1,2 +1,22 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8000;
 var app = express();
+
+// ROUTES
+app.use(express.static('../client/public'));
+var profileRouter = require('./routers/profileRoute');
+app.use('/profile', profileRouter);
+
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// SERVER INITIALIZATION
+app.listen(port, function(err) {
+  console.log('Server is running on port: ', port);
+});
+
+module.exports = app;
