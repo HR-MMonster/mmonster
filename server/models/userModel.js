b533f4e4f5ad6f57e9e8d8b56e137351ce1bfef1
Michael Rico <rico.michael@gmail.com>
2016-04-08 01:04:06 -0700

    Set up group and user game profile schemas

---
+++ server/models/userModel.js
@@ -0,0 +1,56 @@
+var Q = require('q');
+var mongoose = require('mongoose');
+var bcrypt = require('bycrypt-nodejs');
+var SALT_WORK_FACTOR = 10; // required for salt gen
+// N
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  salt: String,
+
+  name: String,
+  email: String,
+  summary: String,
+  photo: String, // store path to file location
+
+});
+
+UserSchema.methods.comparePasswords = function(passwordAttempt) {
+  var savedPassword = this.password;
+  // should return a promise that compares passwords
+  return Q.promise(function(resolve, reject) {
+    bcrypt.compare(passwordAttempt, savedPassword, function(err, isMatch) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(isMatch);
+      }
+    });
+  });
+};
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next(); // perform next middleware action on the controller
+  }
+
+  bycrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    user.salt = salt;
+    next();
+  });
+});
+
+module.exports = mongoose.model('users', UserSchema);
