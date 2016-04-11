2574f0768b15eb4d05f2e74b2af8b12e0d5e769f
Colin Goltra <colin@goltra.co>
2016-04-11 09:47:14 -0700

    building auth factory

--- client/controllers/playerAuth.js
+++ client/controllers/playerAuth.js
@@ -28,3 +28,53 @@ angular.module('app.auth', [])
       });
   };
 });
+
+
+
+.factory('Auth', function ($http, $location, $window) {
+
+  var signin = function () {
+    //POST login data to be matched against databse login info
+    //if success, send to homepage
+    //else if fail, send back to login
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });    
+  };
+
+  var signup = function () {
+    //POST data to be stored into database as new object
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.shortly');
+  };
+
+  var signout = function () {
+    //end session
+    $window.localStorage.removeItem('com.shortly');
+    $location.path('/signin');
+  };
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+
+
+})
