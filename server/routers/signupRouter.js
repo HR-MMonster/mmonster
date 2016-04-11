1964d72a43d6a806aa83cc6904ed9d34aeb3c4e9
David Ogor <doctown@yahoo.com>
2016-04-11 10:50:47 -0700

    Differentiate between user signup and group signup

--- server/routers/signupRouter.js
+++ server/routers/signupRouter.js
@@ -3,9 +3,14 @@
  */
 var signupRouter = require('express').Router();
 var userController = require('../controllers/userController');
+var groupController = require('../controllers/groupController');
 
 signupRouter
-  .route('/')
+  .route('/user')
   .post(userController.createUser);
+
+signupRouter
+  .route('/group')
+  .post(groupController.createGroup);
 
 module.exports = signupRouter;
