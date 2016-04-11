c2076d39ca049bb2981b9ac95641397c1b52684d
Colin Goltra <colin@goltra.co>
2016-04-11 11:15:17 -0700

    signup and signin functionality connecting

--- client/controllers/playerAuth.js
+++ client/controllers/playerAuth.js
@@ -1,6 +1,7 @@
 angular.module('app.auth', [])
 
 .controller('PlayerAuthController', function ($window, $location, Auth) {
+  console.log('can you see this?');
   var PlayerAuthCtrl = this;
   PlayerAuthCtrl.player = {};
 
@@ -9,8 +10,9 @@ angular.module('app.auth', [])
   PlayerAuthCtrl.signin = function () {
     Auth.signin(PlayerAuthCtrl.player)
       .then(function (token) {
+        console.log('inside signin')
         $window.localStorage.setItem('com.app', token);
-        $location.path('./index.html');
+        $location.path('/');
       })
       .catch(function (error) {
         console.error(error);
@@ -20,38 +22,38 @@ angular.module('app.auth', [])
   PlayerAuthCtrl.signup = function () {
     Auth.signup(PlayerAuthCtrl.player)
       .then(function (token) {
+        console.log('logging line 24');
         $window.localStorage.setItem('com.app', token);
-        $location.path('./index.html');
+        $location.path('/');
       })
       .catch(function (error) {
         console.error(error);
       });
   };
-});
-
+})
 
 
 .factory('Auth', function ($http, $location, $window) {
 
-  var signin = function () {
+  var signin = function (user) {
     //POST login data to be matched against databse login info
     //if success, send to homepage
     //else if fail, send back to login
     return $http({
       method: 'POST',
-      url: '/api/users/signin',
+      url: '/signin',
       data: user
     })
     .then(function (resp) {
       return resp.data.token;
-    });    
+    });
   };
 
-  var signup = function () {
+  var signup = function (user) {
     //POST data to be stored into database as new object
     return $http({
       method: 'POST',
-      url: '/api/users/signup',
+      url: '/signup/user',
       data: user
     })
     .then(function (resp) {
@@ -60,12 +62,12 @@ angular.module('app.auth', [])
   };
 
   var isAuth = function () {
-    return !!$window.localStorage.getItem('com.shortly');
+    return !!$window.localStorage.getItem('com.app');
   };
 
   var signout = function () {
     //end session
-    $window.localStorage.removeItem('com.shortly');
+    $window.localStorage.removeItem('com.app');
     $location.path('/signin');
   };
 
@@ -76,5 +78,4 @@ angular.module('app.auth', [])
     signout: signout
   };
 
-
-})
+});
