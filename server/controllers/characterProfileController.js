22f65830bea67dd5a8472740b4d927acf4cbe764
Michael Rico <rico.michael@gmail.com>
2016-04-11 16:09:24 -0700

    Refactor findCharacterProfiles to accept time and dps queries, needs testing

--- server/controllers/characterProfileController.js
+++ server/controllers/characterProfileController.js
@@ -9,17 +9,33 @@ var testData = require('../data/testData').characterProfiles;
 exports.findCharacterProfiles = function(req, res, next) {
   // findAll
   // console.log(req.query);
-  var searchParams = req.query;
+  var userParams = {
+    path: 'user',
+    select: '-password -salt',
+    match: {}
+  };
+
+  if (req.query.dps) {
+    req.query.dps = {$gt: req.query.dps};
+  }
+
+  if (req.query.startTime) {
+    userParams.match.startTime = {$gte: req.query.startTime};
+    delete req.query.startTime;
+  }
+
+  if (req.query.endTime) {
+    userParams.match.endTime = {$lte: req.query.endTime};
+    delete req.query.endTime;
+  }
   // check searchParams for a startTime and endTime
   // iinput these params into the populate
     // take care of overlap of midnight
 
+  var searchParams = req.query;
 
   CharacterProfile.find(searchParams)
-  .populate({
-    path: 'user',
-    select: '-password -salt',
-  })
+  .populate(userParams)
   .exec(function(err, foundProfiles) {
     if (err) {
       console.error('><>< Error quering database for charProfiles:', err, '<><>');
