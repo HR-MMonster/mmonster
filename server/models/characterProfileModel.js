3c26dbbb07a08a4d16eeec1947e53fdb5eab89a3
Michael Rico <rico.michael@gmail.com>
2016-04-12 02:41:50 -0700

    Update endpoints and clean up schema code

--- server/models/characterProfileModel.js
+++ server/models/characterProfileModel.js
@@ -9,10 +9,9 @@ var characterProfileSchema = new Schema({
     type: String,
     required: true
   },
-  user:[userSchema], // should store user reference, look into population with mongoose
-  // gameOptions: [ffxivSchema], // use a nested structue here
-  mic: Boolean, // user has a microphone connection?
-  server: String, // should this be game specific?
+  user:[userSchema],
+  mic: Boolean,
+  server: String,
   dps: Number,
   main: String,
   paladin: Boolean,
@@ -50,18 +49,5 @@ var characterProfileSchema = new Schema({
   A7S: Boolean,
   A8S: Boolean
 });
-
-  /*
-   *  The remaining props of the user userProfile
-   *  can be passed in as props of the model object at initial save.
-   *  This can be decided on the client side and is unopinionated.
-   *  Should include properties specific to the game and relevant user attributes,
-   *  such as for FFXIV:
-   *  { dps     : 78, // damage per second
-   *    main    : "Dark Knight",   //
-   *    jobs    : ["Warrior", "Time Mage", "Red Mage", "Sage"], // store an array of jobs they can play OR a hash if works better for client side
-   *    cleared : {},  // store an object with {levelName1 : boolean, levelName2: boolean, ...}
-   *  }
-   */
 
 module.exports = mongoose.model('CharacterProfile', characterProfileSchema);
--- server/models/groupProfileModel.js
+++ server/models/groupProfileModel.js
@@ -9,15 +9,7 @@ var groupProfileSchema = new Schema({
     required: true
   },
   server: String,
-  group: [groupSchema], // store group ref, see mongoose data population REQUIRED
+  group: [groupSchema],
 });
-
-/*
- *  The rest of the props of the group profile can be passed
- *  in as props of the model object (can be decided on the client side - unopinionated)
- *  This should include properties specific to what the group is looking
- *  for in their ideal candidate that be used to display data on the DOM
- *  or to be queried with to match users gameProfiles.
- */
 
 module.exports = mongoose.model('GroupProfile', groupProfileSchema);
