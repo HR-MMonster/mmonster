470cbd34e7c986a09e474644ff6622062671c497
Travis Baratcart <baratcart.travis@gmail.com>
2016-04-18 15:44:36 -0700

    User and group profile message boards working.

--- client/controllers/Profile.js
+++ client/controllers/Profile.js
@@ -1,15 +1,17 @@
 app.factory('Profile', function($http, $location, $window, Upload) {
-  var urlID = $location.absUrl().split('#');
-  if (urlID.length > 1) {
-    urlID = urlID[1].slice(1);
+  var playerID = $location.absUrl().split('#');
+  if (playerID.length > 1) {
+    playerID = playerID[1].slice(1);
   } else {
-    urlID = $window.localStorage.getItem('id');
+    playerID = $window.localStorage.getItem('id');
   }
+
+  var gameProfileID;
 
   var get = function() {
     return $http({
       method: 'GET',
-      url: '/profile/users/' + urlID
+      url: '/profile/users/' + playerID
     }).then(function(resp) {
       return resp.data;
     });
@@ -18,7 +20,7 @@ app.factory('Profile', function($http, $location, $window, Upload) {
   var update = function(profile) {
     $http({
       method: 'PUT',
-      url: '/profile/users/' + urlID,
+      url: '/profile/users/' + playerID,
       data: profile
     });
     console.log('running update');
@@ -27,16 +29,18 @@ app.factory('Profile', function($http, $location, $window, Upload) {
   var getFFXIV = function() {
     return $http({
       method: 'GET',
-      url: '/profile/users/' + urlID + '/characterProfiles'
+      url: '/profile/users/' + playerID + '/characterProfiles'
     }).then(function(resp) {
-      return resp.data[0];
-    });
+     var FFXIVProfile = resp.data[0];
+     gameProfileID = FFXIVProfile._id;
+     return FFXIVProfile;
+   });
   };
 
   var updateFFXIV = function(profile) {
     $http({
       method: 'PUT',
-      url: '/profile/users/' + urlID + '/characterProfiles/' + profile._id,
+      url: '/profile/users/' + playerID + '/characterProfiles/' + profile._id,
       data: profile
     }).then(function(resp) {
       console.log('FFXIV Profile Updated');
@@ -47,7 +51,7 @@ app.factory('Profile', function($http, $location, $window, Upload) {
 
   var updatePhoto = function(photo) {
     return Upload.upload({
-      url: '/profile/users/' + urlID + '/photos',
+      url: '/profile/users/' + playerID + '/photos',
       data: {userPhoto: photo}
     }).then(function (resp) {
       console.log('Photo Updated');
@@ -57,12 +61,45 @@ app.factory('Profile', function($http, $location, $window, Upload) {
     });
   };
 
+  var postMessage = function(message) {
+    var posterID = $window.localStorage.getItem('id');
+    var posterType = $window.localStorage.getItem('type');
+    if (posterID) {
+      return $http({
+        method: 'POST',
+        url: '/profile/characterProfiles/' + gameProfileID + '/messages',
+        data: {
+          message: {
+            type: posterType,
+            typeId: posterID,
+            content: message
+          }
+        }
+      });
+    }
+  };
+
+  var getMessageAssets = function(message) {
+    var url;
+    var extension = message.type === 'user' ? 'users' : 'groups';
+    $http({
+      method: 'GET',
+      url: '/profile/' + extension + '/' + message.typeId
+    }).then(function(resp) {
+      var posterProfile = resp.data;
+      message.photo = posterProfile.photo;
+      message.name = message.type === 'user' ? posterProfile.username : posterProfile.groupname;
+    });
+  };
+
   return {
     get: get,
     update: update,
     updatePhoto: updatePhoto,
     getFFXIV: getFFXIV,
-    updateFFXIV: updateFFXIV
+    updateFFXIV: updateFFXIV,
+    postMessage: postMessage,
+    getMessageAssets: getMessageAssets
   };
 })
 .controller('ProfileController', ['Profile', function(Profile) {
@@ -106,25 +143,42 @@ app.factory('Profile', function($http, $location, $window, Upload) {
 })
 .controller('FFXIVController', function(Profile) {
   var FFXIVCtrl = this;
-  FFXIVCtrl.profile = Profile.getFFXIV().then(function(profile) {
-    FFXIVCtrl.profile = profile;
+
+  $(document).ready(function() {
+    $('body').on('click', '.profile-redirect', function() {
+      window.location.reload();
+    });
   });
+
+  FFXIVCtrl.getProfile = function() {
+    FFXIVCtrl.profile = Profile.getFFXIV().then(function(profile) {
+      FFXIVCtrl.profile = profile;
+      FFXIVCtrl.profile.messages.forEach(Profile.getMessageAssets);
+    });
+  };
+
+  FFXIVCtrl.getProfile();
+
+  FFXIVCtrl.postMessage = function() {
+    Profile.postMessage(FFXIVCtrl.newMessage)
+    .then(FFXIVCtrl.getProfile);
+  };
 
   FFXIVCtrl.servers = ['Aegis', 'Atomos', 'Carbuncle', 'Garuda', 'Gungnir', 'Kujata', 'Ramuh', 'Tonberry', 'Typhon', 'Unicorn', 'Alexander', 'Bahamut', 'Durandal', 'Fenrir', 'Ifrit', 'Ridill', 'Tiamat', 'Ultima', 'Valefor', 'Yojimbo', 'Zeromus', 'Anima', 'Asura', 'Belias', 'Chocobo', 'Hades', 'Ixion', 'Mandragora', 'Masamune', 'Pandaemonium', 'Shinryu', 'Titan', 'Adamantoise', 'Balmung', 'Cactuar', 'Coeurl', 'Faerie', 'Gilgamesh', 'Goblin', 'Jenova', 'Mateus', 'Midgardsormr', 'Sargatanas', 'Siren', 'Zalera', 'Behemoth', 'Brynhildr', 'Diabolos', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Malboro', 'Ultros', 'Cerberus', 'Lich', 'Moogle', 'Odin', 'Phoenix', 'Ragnarok', 'Shiva', 'Zodiark'];
   FFXIVCtrl.jobs = [
-    {name: 'Paladin', model: 'Paladin'},
-    {name: 'Warrior', model: 'Warrior'},
-    {name: 'Dark Knight', model: 'DarkKnight'},
-    {name: 'White Mage', model: 'WhiteMage'},
-    {name: 'Scholar', model: 'Scholar'},
-    {name: 'Astrologian', model: 'Astrologian'},
-    {name: 'Monk', model: 'Monk'},
-    {name: 'Dragoon', model: 'Dragoon'},
-    {name: 'Ninja', model: 'Ninja'},
-    {name: 'Black Mage', model: 'BlackMage'},
-    {name: 'Summoner', model: 'Summoner'},
-    {name: 'Bard', model: 'Bard'},
-    {name: 'Machinist', model: 'Machinist'}
+  {name: 'Paladin', model: 'Paladin'},
+  {name: 'Warrior', model: 'Warrior'},
+  {name: 'Dark Knight', model: 'DarkKnight'},
+  {name: 'White Mage', model: 'WhiteMage'},
+  {name: 'Scholar', model: 'Scholar'},
+  {name: 'Astrologian', model: 'Astrologian'},
+  {name: 'Monk', model: 'Monk'},
+  {name: 'Dragoon', model: 'Dragoon'},
+  {name: 'Ninja', model: 'Ninja'},
+  {name: 'Black Mage', model: 'BlackMage'},
+  {name: 'Summoner', model: 'Summoner'},
+  {name: 'Bard', model: 'Bard'},
+  {name: 'Machinist', model: 'Machinist'}
   ];
 
   FFXIVCtrl.fights = [
